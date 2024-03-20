a46ad1a410a3b3869b5f1d42248bac37dde3b042
Aleksander Komenda <aleksander.komenda@icloud.com>
2024-03-20 21:12:41 +0100

    Wprowadzenie Do Warsztatów

---
+++ komenda/CarRental/library/include/math_helpers.h
@@ -0,0 +1,10 @@
+//
+// Created by aleks on 13.03.2024.
+//
+
+#ifndef INTRODUCTION_MATH_HELPERS_H
+#define INTRODUCTION_MATH_HELPERS_H
+
+unsigned long long factorial(unsigned int n);
+
+#endif //INTRODUCTION_MATH_HELPERS_H
